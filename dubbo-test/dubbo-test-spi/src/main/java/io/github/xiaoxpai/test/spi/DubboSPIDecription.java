05473c97f23a09f1067cea9f0019c970d97129ee
upxiaoxpai <alf_xiaolh@163.com>
2021-12-19 00:01:24 +0800

    1、Duubbo SPI 两个重要的概念:a扩展点：通过 SPI 机制查找并加载实现的接口;b扩展点实现：实现了扩展接口的实现类。

--- dubbo-test/dubbo-test-spi/src/main/java/io/github/xiaoxpai/test/spi/DubboSPIDecription.java
+++ dubbo-test/dubbo-test-spi/src/main/java/io/github/xiaoxpai/test/spi/DubboSPIDecription.java
@@ -17,6 +17,9 @@ public class DubboSPIDecription {
     //其中 key 被称为扩展名（也就是 ExtensionName），当我们在为一个接口查找具体实现类时，可以指定扩展名来选择相应的扩展实现。例如，这里指定扩展名为 dubbo，Dubbo SPI 就知道我们要使用：org.apache.dubbo.rpc.protocol.dubbo.DubboProtocol 这个扩展实现类，只实例化这一个扩展实现即可，无须实例化 SPI 配置文件中的其他扩展实现类。
     //
     //使用 KV 格式的 SPI 配置文件的另一个好处是：让我们更容易定位到问题。假设我们使用的一个扩展实现类所在的 jar 包没有引入到项目中，那么 Dubbo SPI 在抛出异常的时候，会携带该扩展名信息，而不是简单地提示扩展实现类无法加载。这些更加准确的异常信息降低了排查问题的难度，提高了排查问题的效率。
-    // Dubbo 并没有直接使用 JDK SPI 机制，而是借鉴其思想，实现了自身的一套 SPI 机制
+
+    //Dubbo SPI 两个重要的概念：
+    //扩展点：通过 SPI 机制查找并加载实现的接口（又称“扩展接口”）。前文示例中介绍的 Log 接口、com.mysql.cj.jdbc.Driver 接口，都是扩展点。
+    //扩展点实现：实现了扩展接口的实现类。
 
 }
